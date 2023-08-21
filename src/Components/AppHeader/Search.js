aaba2afefe035bcdc5fa17a0656cbc0ae1c17fb3
anz-yogesh <142186259+anz-yogesh@users.noreply.github.com>
2023-08-22 09:19:10 +1200

    Add files via upload

---
+++ src/Components/AppHeader/Search.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { SearchBox } from "@fluentui/react-search-preview";
+
+export const Search = (props) => {
+  return (
+    <div className="Search">
+      {" "}
+      {/* Apply the class here */}
+      <SearchBox
+        style={{
+          maxWidth: "944px",
+          width: "100%",
+        }}
+        placeholder="Look for Trends, Messages, Documents and More!"
+        {...props}
+      />
+    </div>
+  );
+};
+
+// const argTypes = {
+//   placeholder: {
+//     description:
+//       "Placeholder text for the SearchBox. If using this instead of a label (which is " +
+//       "not recommended), be sure to provide an `aria-label` for screen reader users.",
+//     type: { name: "string", required: false },
+//     table: { type: { summary: "string" } },
+//   },
+//   disabled: {
+//     description: "Whether the SearchBox is disabled",
+//     type: { name: "boolean", required: false },
+//     table: { type: { summary: "boolean" } },
+//   },
+//   children: { table: { disable: true } },
+//   as: { table: { disable: true } },
+// };
---
+++ src/Views/Login/Login.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import {
+  Button,
+  Label,
+  Input,
+  useId,
+  Divider,
+} from "@fluentui/react-components";
+import { Open16Regular } from "@fluentui/react-icons";
+import { Link, NavLink } from "react-router-dom";
+import { Card, CardProps, CardFooter } from "@fluentui/react-components";
+import "./Login.css";
+
+const ASSET_URL =
+  "https://raw.githubusercontent.com/microsoft/fluentui/master/packages/react-components/react-card/stories/assets/";
+
+const Login: React.FC<CardProps> = (props) => {
+  const emailId = useId("input-email");
+  const passwordId = useId("input-password");
+
+  return (
+    <div className="loginContainer">
+      <Card className="card" {...props}>
+        <img
+          src={`${ASSET_URL}sales_template.png`}
+          alt="Sales Presentation Preview"
+        />
+        <div className="loginRoot">
+          <Label htmlFor={emailId}>Email</Label>
+          <Input type="email" id={emailId} placeholder="user@example.com" />
+          <Label htmlFor={passwordId}>Password</Label>
+          <Input type="password" id={passwordId} placeholder="********" />
+          <div className="buttonGroup">
+            <Button appearance="primary">Submit</Button>
+            <Button appearance="outline">Cancel</Button>
+          </div>
+          <div className="insetstyle">
+            <Divider inset>Continue With</Divider>
+          </div>
+          <CardFooter className="footer">
+            <Button appearance="primary" icon={<Open16Regular />}>
+              Google
+            </Button>
+            <Button appearance="primary" icon={<Open16Regular />}>
+              Microsoft
+            </Button>
+          </CardFooter>
+          <p className="signUpText">
+            Don't have an account?
+            <Link to={"/signUp"}> Sign up!</Link>
+          </p>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
